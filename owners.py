20123c6480c6383287c909bdcc1543c37649d50f
Sofiane <oumsof@gmail.com>
2021-12-19 11:28:13 -0800

    take into account duplicated entries

--- owners.py
+++ owners.py
@@ -4,26 +4,22 @@ import json
 @click.command()
 @click.argument('filename', type=click.Path(exists=True))
 @click.argument('mints', type=click.Path(exists=True))
-@click.option('--data', default='=', help='example --data=mintWalletAddress')
+@click.option('--data', default='=', help='example --data=owner_wallet')
 
 def parse(filename, mints, data):
     """Simple program that greets NAME for a total of COUNT times."""
     print(f"parsing: ", mints)
     f = open(mints)
-    s = set()
+
+    f2 = open(filename,)
+    json_data = json.load(f2)
 
     lines = f.readlines()
     for l in lines:
-        s.add(l.strip())
-
-    print(f"parsing: ", filename)
-    f = open(filename,)
-    json_data = json.load(f)
-
-    for item in json_data:
-        if s.__contains__(item['mint_account']):
-            print(item[data])
-
+        mint = l.strip()
+        for item in json_data:
+            if item['mint_account'] == mint:
+                print(mint, ",", item[data])
 
 if __name__ == '__main__':
     parse()
